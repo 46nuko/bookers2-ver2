6e2bc2256cbf9dd4f60b8a82dd5022c579dd8778
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-02-17 02:25:57 +0000

    bootstrap実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
-
   def edit
     @user = User.find(params[:id])
   end
-def update
-  @user = User.find(params[:id])
-  @user.update(user_params)
-  redirect_to user_path(@user)
-end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
 
 def index
   @user = User.new
