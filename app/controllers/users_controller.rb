1bb471e37d67979b7aef7c9e049c553bb71e0178
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-02-24 02:55:32 +0000

    rspecエラーつぶし中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ def index
   @user = User.new
   @users = User.all
   @book = current_user
+  @user = current_user
 end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
+  root to: "home#top"
   devise_for :users
 
   resources :books, only: [:index, :show, :create, :destroy, :edit, :update]
   resources :users, only: [:index, :show, :create, :edit, :update]
-  get "/homes/about" => "homes#about", as:"about"
+  get '/home/about', to: 'home#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
